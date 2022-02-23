2083104f422b66f094cc9e2c0e10a8cc6cf90a48
Peter Mwansa <mwansapeter800@gmail.com>
2022-02-23 17:36:11 +0300

    Add the other pages

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-$(document).ready(function(){
+const express = require("express");
+const app = express();
 
-    //to display the inquiries modal when clicked
-    $("#signupButton").click(function(){
-        $("#signupModal").modal('show');
-    });
-    
-    //this will close the modal when clicked
-    $("#cancelButton").click(function(){
-        $("#signupModal").modal('hide');
-    });
 
-    //to cancel the login modal when the x is clicked
-    $("#crossModal").click(function(){
-        
-        $("#signupModal").modal('hide');
-    
-    });
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
 });
+
+
+
+// $(document).ready(function(){
+
+//     //to display the inquiries modal when clicked
+//     $("#signupButton").click(function(){
+//         $("#signupModal").modal('show');
+//     });
+    
+//     //this will close the modal when clicked
+//     $("#cancelButton").click(function(){
+//         $("#signupModal").modal('hide');
+//     });
+
+//     //to cancel the login modal when the x is clicked
+//     $("#crossModal").click(function(){
+        
+//         $("#signupModal").modal('hide');
+    
+//     });
+// });
+
+
+
+
+// app.listen(3000, function(){
+//     console.log("Server started on port 3000");
+// });
